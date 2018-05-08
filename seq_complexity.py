eaa6b93a694101f364130d96635376b08c730735
tanjil <tanjil_ahmed@hotmail.com>
2018-05-08 14:57:06 -0400

    Final Version

--- seq_complexity.py
+++ seq_complexity.py
@@ -1,3 +1,11 @@
+#!/usr/bin/python
+#title           :seq_complexity.py
+#description     :This will calculate linguistic complexity a DNA sequences in a file.
+#author          :Tanjil Ahmed
+#date            :05/08/2018
+#version         :1.0
+#usage           :python seq_complexity.py <filename.fasta>
+#==============================================================================
 import sys
 import pandas as pd
 import os
@@ -13,14 +21,15 @@ class Seq(object):
             if not kmer in counters:
                 counters[kmer]=0
             counters[kmer]+=1
+        return counters
     def pdf(self,ksize,observed_kmer,possible_kmer,df):
         global row
         proportion=observed_kmer/possible_kmer
         df[row]=[ksize,observed_kmer,possible_kmer,proportion]
         row=row+1
     def plot(self,df,fname):
-        print(df)
-        plt.plot(df['ksize'],df['Observed_proportion'])
+        #print(df)
+        plt.plot(df['ksize'][1:50],df['Observed_proportion'][1:50])
         plt.xlabel('Kmer Size')
         plt.ylabel('Proportion of observed Kmer')
         plt.title(fname)
@@ -31,69 +40,83 @@ class Seq(object):
         if not os.path.exists(path):
             os.makedirs(path)
         gname=os.path.join(path,gname)
+        plt.text(15,.1,'Propotion of observed kmer is 1 for kmer size>10')
         plt.savefig(gname)
-        #plt.show()
-        #print(df['ksize'])
-    def ling_complexity(self,val1,val2):
-        complexity=val1/val2
+        plt.clf()
+
+    def ling_complexity(self,df):
+        column1_sum=df['observed_kmers'].sum()
+        column2_sum=df['possible_kmers'].sum()
+        complexity=column1_sum/column2_sum
         return complexity
 
 
-f = open(sys.argv[1],"r")
-fasta = f.readlines()
-lc=pd.DataFrame()
-row_lc=0
-dir = os.getcwd()
-dffolder='\\Panda DataFrame'
-lcfolder='\\Linguistic Complexity'
-plotfolder='Plot'
-for line_number,line in enumerate(fasta):
-    if line_number%3==0:
-        fname=line.strip('>')
-        fname=fname.strip('\n')
-        print(fname)
-    if line_number%3==1:
-        row=0
-        df=pd.DataFrame()
-        sequence=line.rstrip()
-        print(sequence)
-        seq=Seq(sequence)
-        for j in range(1,len(sequence)+1):
-            counters={}
-            seq.count_kmers(j,counters)
-            observed_kmer=len(counters)
-            if j==1:
-                possible_kmer=4**j
-            else:
-                possible_kmer=len(sequence)-j+1
-            seq.pdf(j,observed_kmer,possible_kmer,df)
-            #print(row)
 
-        #print(counters)
-        #seq.pdf()
-        df=df.T
-        df.columns=['ksize','observed_kmers','possible_kmers','Observed_proportion']
-        filename=fname+".txt"
-        #dir1 = os.path.join(dir,dffolder)
-        #print(dir1)
-        dfpath=dir+dffolder
-        if not os.path.exists(dfpath):
-            os.makedirs(dfpath)
-        filename=os.path.join(dfpath,filename)
-        df.to_csv(filename, index=False, sep=' ', header=True)
-        #print(df)
-        column1_sum=df['observed_kmers'].sum()
-        column2_sum=df['possible_kmers'].sum()
-        print(column1_sum)
-        print(column2_sum)
-        seq.plot(df,fname)
-        lcomplexity=seq.ling_complexity(column1_sum,column2_sum)
-        lc[row_lc]=[fname,lcomplexity]
-        row_lc=row_lc+1
-lc=lc.T
-lc.columns=['Species','Linguistic Complexity']
-lcpath=dir+lcfolder
-if not os.path.exists(lcpath):
-    os.makedirs(lcpath)
-lcfile=os.path.join(lcpath,"linguistic_complexity.txt")
-lc.to_csv(lcfile, index=False, sep=' ', header=True)
+def file_format_check(fasta):
+        string="ACTGRYM"
+        for line_number,line in enumerate(fasta):
+            if line_number%3==1:
+                sequence=line.rstrip()
+                if any(c not in string for c in sequence):
+                    return 0
+        return 1
+
+
+if __name__=="__main__":
+    if len(sys.argv) == 1:
+        f=open("nd2.fasta","r")
+    else:
+        f = open(sys.argv[1],"r")
+    fasta = f.readlines()
+    lc=pd.DataFrame()
+    row_lc=0
+    dir = os.getcwd()
+    dffolder='\\Panda DataFrame'
+    lcfolder='\\Linguistic Complexity'
+    plotfolder='\\Plot'
+    if file_format_check(fasta)==True:
+        print("File contains appropriate character")
+        for line_number,line in enumerate(fasta):
+            if line_number%3==0:
+                fname=line.strip('>')
+                fname=fname.strip('\n')
+                print(fname)
+            if line_number%3==1:
+                row=0
+                df=pd.DataFrame()
+                sequence=line.rstrip()
+                sequence=sequence.replace("R","A")
+                sequence=sequence.replace("Y","T")
+                sequence=sequence.replace("M","C")
+                seq=Seq(sequence)
+                for j in range(1,len(sequence)+1):
+                    counters={}
+                    seq.count_kmers(j,counters)
+                    observed_kmer=len(counters)
+                    if j==1:
+                        possible_kmer=4**j
+                    else:
+                        possible_kmer=len(sequence)-j+1
+                    seq.pdf(j,observed_kmer,possible_kmer,df)
+
+                df=df.T
+                df.columns=['ksize','observed_kmers','possible_kmers','Observed_proportion']
+                filename=fname+".txt"
+                dfpath=dir+dffolder
+                if not os.path.exists(dfpath):
+                    os.makedirs(dfpath)
+                filename=os.path.join(dfpath,filename)
+                df.to_csv(filename, index=False, sep=' ', header=True)
+                seq.plot(df,fname)
+                lcomplexity=seq.ling_complexity(df)
+                lc[row_lc]=[fname,lcomplexity]
+                row_lc=row_lc+1
+        lc=lc.T
+        lc.columns=['Species','Linguistic Complexity']
+        lcpath=dir+lcfolder
+        if not os.path.exists(lcpath):
+            os.makedirs(lcpath)
+        lcfile=os.path.join(lcpath,"linguistic_complexity.txt")
+        lc.to_csv(lcfile, index=False, sep=' ', header=True)
+    else:
+        print ("File doesn't contain appropriate base")
